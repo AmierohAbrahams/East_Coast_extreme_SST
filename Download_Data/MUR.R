49d9b939ec0457460c2945adef3dc749e6436fb0
AmierohAbrahams <3559244@myuwc.ac.za>
2021-03-12 13:52:37 +0200

    Improved MUR

--- Download_Data/MUR.R
+++ Download_Data/MUR.R
@@ -1,52 +1,54 @@
-# MUR_1km_download.R
-# The purpose of this file is to download super-high-res MUR 1 KM data
+# The packages we will need
+# install.packages("dplyr")
+# install.packages("lubridate")
+# install.packages("ggplot2")
+# install.packages("tidync")
+# install.packages("doParallel")
+# install.packages("rerddap")
+# install.packages("plyr") # Note that this library should never be loaded, only installed
 
-# Setup -------------------------------------------------------------------
+# The packages we will use
+library(dplyr) # A staple for modern data management in R
+library(lubridate) # Useful functions for dealing with dates
+library(ggplot2) # The preferred library for data visualisation
+library(tidync) # For easily dealing with NetCDF data
+library(rerddap) # For easily downloading subsets of data
+library(doParallel) # For parallel processing
 
-.libPaths(c("~/R-packages", .libPaths()))
-library(tidyverse)
-library(lubridate)
-library(tidync)
-library(doParallel)
-registerDoParallel(cores = 50)
-# library(ncdf4)
-
-# File location
-base_URL <- "https://podaac-opendap.jpl.nasa.gov/opendap/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1"
-
-# Download ----------------------------------------------------------------
-
-# Function for downloading a given subset
-download_MUR_1km <- function(file_date, lon_range, lat_range){
-  
-  file_name <- paste0(base_URL,"/",year(file_date),"/",yday(file_date),"/",
-                      gsub("-", "", file_date),"090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc")
-  suppressWarnings( 
-    MUR_dat <- tidync(file_name) %>%
-      hyper_filter(lon = between(lon, lon_range[1], lon_range[2]),
-                   lat = between(lat, lat_range[1], lat_range[2])) %>% 
-      hyper_tibble() %>% 
-      select(lon, lat, time, analysed_sst) %>% 
-      dplyr::rename(t = time, temp = analysed_sst) %>% 
-      na.omit() %>% 
-      mutate(t = as.Date(as.POSIXct(t, origin = "1981-01-01 00:00:00.0")),
-             temp = temp-273.15) # Convert from K to C
-  )
-  return(MUR_dat)
+MUR_sub_dl <- function(time_df){
+  MUR_dat <- griddap(x = "jplMURSST41", 
+                       url = "https://coastwatch.pfeg.noaa.gov/erddap/", 
+                       time = c(time_df$start, time_df$end), 
+                       #zlev = c(0, 0),
+                       latitude = c(-37.5, -27.5),
+                       longitude = c(20, 35),
+                       fields = "analysed_sst")$data %>% 
+    mutate(time = as.Date(stringr::str_remove(time, "T00:00:00Z"))) %>% 
+    dplyr::rename(t = time, temp = analysed_sst) %>% 
+    select(lon, lat, t, temp) %>% 
+    na.omit()
 }
 
-# Download all dates
-MUR_EC <- plyr::ldply(seq(as.Date("2002-06-01"), as.Date("2021-03-04"), by = "day"), download_MUR_1km,
-                       .parallel = F, lon_range = c(20, 35), lat_range = c(-37.5, -27.5))
 
-write_csv(MUR_EC, "Data/MUR_EC.csv")
+dl_years <- data.frame(date_index = 1:1,
+                       start = as.Date(c("2002-06-01")),
+                       end = as.Date(c("2021-03-05")))
+system.time(
+  MUR_data <- dl_years %>% 
+    group_by(date_index) %>% 
+    group_modify(~MUR_sub_dl(.x)) %>% 
+    ungroup() %>% 
+    select(lon, lat, t, temp)
+) 
 
-# Test visual
-ggplot(data = filter(MUR_EC, t == "2002-06-01"), aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = temp)) +
-  # borders() +
-  scale_fill_viridis_c()
+save(MUR_data, file = "data/MUR.RData")
 
-
-
-
+MUR_data %>% 
+  filter(t == "2003-06-01") %>% 
+  ggplot(aes(x = lon, y = lat)) +
+  geom_tile(aes(fill = temp)) +
+  # borders() + # Activate this line to see the global map
+  scale_fill_viridis_c() +
+  coord_quickmap(expand = F) +
+  labs(x = NULL, y = NULL, fill = "SST (°C)") +
+  theme(legend.position = "bottom")
